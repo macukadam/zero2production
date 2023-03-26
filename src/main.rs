77bb14ee7adb19d557c6e8badf29aed28fb5d894
Ugurcan Akpulat <ugurcan.akpulat@gmail.com>
2023-03-26 14:59:28 +0200

    Fmt fix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
